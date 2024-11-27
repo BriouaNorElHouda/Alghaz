979d9a94a69c758e16774995d2f8242b015a5d07
Nor el houda <nor.el.houda.brioua@gmail.com>
2024-11-27 19:52:03 +0100

    Add project files.

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Alghaz.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
---
+++ Models/Alghaz.cs
@@ -0,0 +1,14 @@
+﻿namespace Alghaz.Models
+{
+    public class Alghaz
+    {
+        public int Id { get; set; }
+        public string LoghzQuestion { get; set; }
+        public string LoghzAnswer { get; set; }
+
+        public Alghaz()
+        {
+                
+        }
+    }
+}
