c71f2b0dee7213c0f2068ccb7e820402114538db
BriouaNorElHouda <84919515+BriouaNorElHouda@users.noreply.github.com>
2024-12-11 21:17:16 +0100

    Update HomeController.cs

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -7,7 +7,7 @@ namespace Alghaz.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
-
+//use log
         public HomeController(ILogger<HomeController> logger)
         {
             _logger = logger;
